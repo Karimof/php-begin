3ae37955bf7f87d9e356dfcb66a89ca993cbe2f9
Islom <islamkarimov8580@gmail.com>
2019-02-15 17:01:58 +0500

    2-zadacha

--- Zadacha_2(if).php
+++ Zadacha_2(if).php
@@ -2,13 +2,40 @@
 	<head>
 		<body> 
 			<?php
-				$massiv = array( 22, 'Islom', 'dasturchi', true);
-				if($massiv[0]<=40 && $massiv[3]===true){
-					echo '<h3 style="font-family:calibri; border: 1px solid green; width:20%; text-align:center">Yaroqli</h3>';
-				}	
-				else {
-					echo '<h3 style="font-family:calibri; border: 1px solid red; width:20%; text-align:center">Yaroqsiz!</h3>';
-				}	
+				$input = 'doctor' ;
+				$massiv1 = array( 22, 'Islom',  'dasturchi' , true );
+				$massiv2 = array( 25, 'Sarvar',  'injiner' , true );
+				$massiv3 = array( 22, 'Shohrux',  'doctor' , true );
+				$bool=false;
+				if($input == $massiv1[2] && $massiv1[0] <= 40 && $massiv1[3]===true){
+					$bool=true;
+					if($bool==true){
+						echo  $massiv1[1] . ' Yaroqli';
+					}
+					else {
+						echo $massiv1[1] . ' Yaroqsiz!';
+					}
+				}
+			
+				if($input == $massiv2[2] && $massiv2[0] <= 40 && $massiv2[3]===true){
+					$bool=true;
+					if($bool){
+						echo  $massiv2[1] . ' Yaroqli';
+					}
+					else {
+						echo $massiv2[1] . ' Yaroqsiz!';
+					}
+				}
+				
+				if($input == $massiv3[2] && $massiv3[0] <= 40 && $massiv3[3]===true){
+					$bool=true;
+					if($bool){
+						echo  $massiv3[1] . ' Yaroqli';
+					}
+					else {
+						echo $massiv3[1] . ' Yaroqsiz!';
+					}
+				}
 			?>
 		</body>
 	</head>
