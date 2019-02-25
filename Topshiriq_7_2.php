0b40a78682bcd6060b59a756b3a497b7be87d3af
Islom <islamkarimov8580@gmail.com>
2019-02-25 09:24:47 +0500

    7-topshiriq 2-chisi (modified)

--- Topshiriq_7_2.php
+++ Topshiriq_7_2.php
@@ -15,11 +15,16 @@
 							echo '<table style="border: 1px solid; padding: 5px">
 								<tr>
 									<td>
-										<input style="width: 400px; margin: 5px" placeholder="savol" type="text" name="savolNames[]"> <br>
+										<input style="width: 450px; margin: 5px" placeholder="savol" type="text" name="savolNames[]"> <br>
 										<input style="width:95px; margin: 5px" placeholder ="variant a)" type="text" name="variantsA[]">
 										<input style="width:95px; margin: 5px" placeholder ="variant b)" type="text" name="variantsB[]">
 										<input style="width:95px; margin: 5px" placeholder ="variant c)" type="text" name="variantsC[]">
-										<input style="width:72px; margin: 5px; border: 2px solid green" placeholder ="to\'g\'ri javob" type="text" name="javobNames[]">
+										&nbsp&nbspTo\'g\'ri javob:
+										<select name="javoblar[]">
+											<option value="a">a</option>
+											<option value="b">b</option>
+											<option value="c">c</option>
+										</select>
 									</td>
 								</tr>
 							</table>';
--- uploads_7_2.php
+++ uploads_7_2.php
@@ -6,26 +6,30 @@
 				$variantsA = $_POST['variantsA'];
 				$variantsB = $_POST['variantsB'];
 				$variantsC = $_POST['variantsC'];
-				$javobNames = $_POST['javobNames'];
-				if($savolNames && $javobNames && $variantsA && $variantsB && $variantsC) {
+				$javoblar = $_POST['javoblar'];
+				if($savolNames  && $variantsA && $variantsB && $variantsC) {
 					$fileName = 'test.txt';
 					$file = fopen($fileName, "w");
 					$bool = false;
 					foreach ($savolNames as $key => $value) {
-						if($savolNames[$key] && $javobNames[$key] && $variantsA[$key] && $variantsB[$key] && $variantsC[$key]){
-							$yozishgaTayyor .= $savolNames[$key] . '; ' . $variantsA[$key] . '; ' . $variantsB[$key] . '; ' . $variantsC[$key] . '; ' . $javobNames[$key] . ";\n";
+						if($savolNames[$key]  && $variantsA[$key] && $variantsB[$key] && $variantsC[$key]){
+							fwrite($file, $savolNames[$key] . '; ');
+							fwrite($file, $variantsA[$key] . '; ');
+							fwrite($file, $variantsB[$key] . '; ');
+							fwrite($file, $variantsC[$key] . '; ');
+							fwrite($file, $javoblar[$key] . ";\n");
 							$bool = true;
 						}
 					}
 					if($bool ==true){
 						echo 'Ma\'lumotlar saqlandi';
-						fwrite($file, $yozishgaTayyor);
 					}
+					fclose($file);
 				}	
 				else {
-					echo 'nimadir yozing ahir!';
+					echo 'Ma\'lumotlarni to\'liq kiriting!';
 				}
-				fclose($file);
+				
 			?>
 		</body>
 	</head>
